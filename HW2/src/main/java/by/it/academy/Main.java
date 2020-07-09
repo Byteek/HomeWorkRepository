4baba6797fbae56e144e1abe3967e21b268df397
Nikita_Pohodski <byteek.p.n@gmail.com>
2020-07-09 12:31:37 +0300

    Home work 2 (MySQL)

---
+++ HW2/src/main/java/by/it/academy/Main.java
@@ -0,0 +1,48 @@
+package by.it.academy;
+
+import java.sql.*;
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String dbURL = "jbdc:mysql://localhost:3306/mydb";
+
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Properties properties = new Properties();
+            properties.put("user", "root");
+            properties.put("password", "root");
+            properties.put("useSSL", "false");
+            properties.put("serverTimezone", "UTC");
+
+            Connection myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb",properties);
+            System.out.println("Is connected: " + !myConnection.isClosed());
+
+            Statement statement = myConnection.createStatement();
+
+            String query = "select paydate, value, name from mydb.expenses, mydb.receivers where receiver=receivers.num";
+
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()){
+                System.out.println(
+                        resultSet.getString(1) + " " +
+                                resultSet.getString(2) + " " +
+                                resultSet.getString(3) + " "
+                );
+
+            }
+            myConnection.close();
+        }catch (ClassNotFoundException | SQLException e){
+            System.out.println("Error loading driver: " + e);
+        }finally {
+
+            System.out.println("Finally line");
+        }
+
+
+
+
+    }
+}
